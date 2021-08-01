08b5a9846ad7870dab5d74a617885089561b3370
Matt Weaver <j.matthew.weaver@gmail.com>
2021-08-01 17:15:33 -0400

    Core module

---
+++ core/src/test/java/com/iw/tpg/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iw.tpg.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
